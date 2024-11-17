0520bba93a29a3a620e2e3015be67b76515a15f7
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-17 13:52:22 +0530

    user input

---
+++ prompt.js
@@ -0,0 +1,10 @@
+// how to get user input
+
+//  let name = prompt("whats your name: ")
+// console.log("hi", name)
+
+// let tickets =prompt ('how many tickets do you want? ')
+// console.log("you shoud pay", Number(tickets) * 20)
+
+let a = prompt('enter a number')
+console.log(Number(a) + 10)

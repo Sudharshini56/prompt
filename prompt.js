bddd3c13d343dde76ecf2c7213840e45c6f1e7a3
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-17 22:23:35 +0530

    created convert celsius to fahrenheit

--- prompt.js
+++
@@ -1,7 +0,0 @@
-// //Get your input
-
-// let user = prompt("Enter your name: ")
-// console.log("My Name is", user)
-
-let sets = prompt("How many sets do you need: ")
-console.log("you have to pay: ", Number(sets) * 650, "Rs")
